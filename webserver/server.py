4d68119915b2420c871a48d9452f88b0606149f0
Jacques Van Anh <vajacques@gmail.com>
2016-11-11 14:34:07 +0000

    navigation bar

--- webserver/server.py
+++ webserver/server.py
@@ -332,6 +332,7 @@ def login():
 
 @app.route('/user/<username>')
 def profile(username):
+    #username = session['username']
     name = get_name_from_username()
     stations = get_stations_for_user()
     favs = get_song_favs_for_user()
@@ -382,6 +383,38 @@ def signup():
         return render_template('signup.html')
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
+import datetime
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """convert a datetime to a different format."""
+    return value.strftime(format)
+
+app.jinja_env.filters['datetimefilter'] = datetimefilter
+
+@app.route("/template")
+def template_test():
+    return render_template('template.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="Index", current_time=datetime.datetime.now())
+
+@app.route("/home")
+def home():
+    # return render_template('template.html', my_string="Foo", 
+    #     my_list=[6,7,8,9,10,11], title="Home", current_time=datetime.datetime.now())
+    return redirect(url_for('profile', username=session['username']))
+
+@app.route("/about")
+def about():
+    return render_template('template.html', my_string="Bar", 
+        my_list=[12,13,14,15,16,17], title="About", current_time=datetime.datetime.now())
+
+@app.route("/contact")
+def contact():
+    return render_template('template.html', my_string="FooBar"
+        , my_list=[18,19,20,21,22,23], title="Contact Us", current_time=datetime.datetime.now())
+
+
 
 
 #
